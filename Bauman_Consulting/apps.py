6b736cfac5a056ae7b7dcf8d2880a3d377202f87
xkhadmy <dimaadboos@gmail.com>
2024-08-11 08:43:16 +0200

    add index Bauman_Consulting

---
+++ Bauman_Consulting/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BaumanConsultingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Bauman_Consulting'
---
+++ Bauman_Consulting/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = 'Bauman_Consulting'
+
+urlpatterns = [
+    path('',index, name='index')
+]
