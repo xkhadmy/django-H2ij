12a111897550a0d0ee4271ce96dfb020c3c50d5c
xkhadmy <dimaadboos@gmail.com>
2024-08-11 22:27:25 +0200

    Unser_Angebot

--- Bauman_Consulting/urls.py
+++ Bauman_Consulting/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import index
 from .views import (
-
+    page_Unser_Angebot,
     page_Kontakt,
     
 )
@@ -10,7 +10,8 @@ app_name = 'Bauman_Consulting'
 
 urlpatterns = [
     path('',index, name='index'),
-    
+    path("Unser_Angebot/", page_Unser_Angebot, name="Unser_Angebot"),
     path("Kontakt/", page_Kontakt, name="Kontakt"),
+    
     
 ]
