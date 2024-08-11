458fc017f471a6c7f5b7e10410f517b0783e54a9
xkhadmy <dimaadboos@gmail.com>
2024-08-11 22:07:38 +0200

    Kontakt

--- Bauman_Consulting/urls.py
+++ Bauman_Consulting/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from .views import index
+from .views import (
+
+    page_Kontakt,
+    
+)
+
 app_name = 'Bauman_Consulting'
 
 urlpatterns = [
-    path('',index, name='index')
+    path('',index, name='index'),
+    
+    path("Kontakt/", page_Kontakt, name="Kontakt"),
+    
 ]
--- Bauman_Consulting/views.py
+++ Bauman_Consulting/views.py
@@ -4,3 +4,34 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'Home.html' )
+
+def page_Unterstutzung(request):
+    return render(request, "utils/buttons/Unterstutzung.html")
+
+
+def page_Referenzen(request):
+    return render(request, "utils/buttons/Referenzen.html")
+
+
+def page_Literatur(request):
+    return render(request, "utils/buttons/Literatur.html")
+
+
+def page_Kontakt(request):
+    return render(request, "utils/buttons/Kontakt.html")
+
+
+def page_Unser_Angebot(request):
+    return render(request, "utils/buttons/Unser_Angebot.html")
+
+
+def page_Bestellen(request):
+    return render(request, "utils/buttons/Bestellen.html")
+
+
+def page_Organisatorisches_Sicherheitskonzept(request):
+    return render(request, "utils/buttons/Organisatorisches_Sicherheitskonzept.html")
+
+
+def page_Technisches_Sicherheitskonzept(request):
+    return render(request, "utils/buttons/Technisches_Sicherheitskonzept.html")
