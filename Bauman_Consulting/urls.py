8d9b2dbe6d64393894fcf3913aee3186a51d6578
xkhadmy <dimaadboos@gmail.com>
2024-08-14 18:32:49 +0200

    url up2

--- Bauman_Consulting/urls.py
+++ Bauman_Consulting/urls.py
@@ -3,7 +3,8 @@ from .views import index
 from .views import (
     page_Unser_Angebot,
     page_Kontakt,
-    
+    page_Organisatorisches_Sicherheitskonzept,
+    page_Technisches_Sicherheitskonzept
 )
 
 app_name = 'Bauman_Consulting'
@@ -12,6 +13,15 @@ urlpatterns = [
     path('',index, name='index'),
     path("Unser_Angebot/", page_Unser_Angebot, name="Unser_Angebot"),
     path("Kontakt/", page_Kontakt, name="Kontakt"),
+    path("Organisatorisches_Sicherheitskonzept/",
+        page_Organisatorisches_Sicherheitskonzept,
+        name="Organisatorisches_Sicherheitskonzept",
+    ),
+    path(
+        "Technisches_Sicherheitskonzept/",
+        page_Technisches_Sicherheitskonzept,
+        name="Technisches_Sicherheitskonzept",
+    ),
     
     
 ]
