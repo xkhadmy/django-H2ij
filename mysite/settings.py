cbb5ea45baccfe98594418a9496efafcb64e64a1
xkhadmy <dimaadboos@gmail.com>
2024-08-16 09:44:50 +0200

    decouple

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,16 +22,15 @@ load_dotenv(BASE_DIR / ".env")
 # Загрузка переменных среды из файла .env
 load_dotenv()
 
-# Вывод значений переменных среды для отладки
-print("EMAIL_HOST_USER:", os.getenv("EMAIL_HOST_USER"))
-print("EMAIL_HOST_PASSWORD:", os.getenv("EMAIL_HOST_PASSWORD"))
+ 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+# Определение параметров SMTP из переменных окружения
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "cauto1919@gmail.com"  # Ваш адрес Gmail
-EMAIL_HOST_PASSWORD = "rlnkhsifemamcqzk"  # Пароль от вашего Gmail аккаунта
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")  # Используем переменную окружения
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")  # Используем переменную окружения
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 # SECURITY WARNING: keep the secret key used in production secret!
