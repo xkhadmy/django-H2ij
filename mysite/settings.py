61ff8477bf16d381c757eb893dc8820adf3959d2
xkhadmy <dimaadboos@gmail.com>
2024-08-16 09:17:42 +0200

    CSRF_TRUSTED_ORIGINS

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,9 @@ ALLOWED_HOSTS = ["*"]
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
 # CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    'https://www.baumann-consulting-sulz.ch',
+]
 
 # Application definition
 
